e09f0fe9dba2ad667235665b25a87cf49ef914ef
chawkitariq <chawkitariq@icloud.com>
2025-04-27 21:41:03 +0200

    improve environment configuration

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://ec2-56-228-27-249.eu-north-1.compute.amazonaws.com/',
+    apiAssetsUrl: 'https://ec2-56-228-27-249.eu-north-1.compute.amazonaws.com//assets',
+    defaultItemImage: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg'
+};
